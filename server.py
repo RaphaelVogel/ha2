897d67024852d12612d6808a7b47a56813095bc8
Raphael Vogel <raphael.vogel@web.de>
2015-02-14 19:59:36 +0100

    access modules

--- server.py
+++ server.py
@@ -3,13 +3,15 @@ from bottle import route, run, static_file
 
 @route('/')
 def home():
-	return static_file('index.html', root='./')
+	return static_file('index.html', root='./web')
 
 @route('/static/<filepath:path>')
 def serve_static(filepath):
-    return static_file(filepath, root='./static')
-	
+    return static_file(filepath, root='./web/static')
+
+
 if len(sys.argv) > 1 and sys.argv[1] == 'devmode':
 	run(host='localhost', port=8080, debug=True, reloader=True)
 else:
 	run(server='cherrypy' ,host='localhost', port=8080, debug=False, reloader=False)
+
