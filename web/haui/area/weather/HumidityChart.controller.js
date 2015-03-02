7cfdee0c5fead4e08911e48cea2d51bfca8811f3
Raphael Vogel <raphael.vogel@web.de>
2015-03-02 22:15:55 +0100

    split put into different charts

---
+++ web/haui/area/weather/HumidityChart.controller.js
@@ -0,0 +1,23 @@
+﻿sap.ui.controller("haui.area.weather.HumidityChart", {
+    year: new Date().getFullYear(),
+	month: new Date().getMonth() + 1,
+	day: new Date().getDate(),
+
+	onInit: function() {
+	    if(this.day < 10){
+		    this.day = "0"+this.day;
+		}
+		if(this.month < 10){
+			this.month = "0"+this.month;
+		}
+        var tempChart = this.getView().byId("humidityChart");
+		var tempModel = new sap.ui.model.json.JSONModel();
+		tempModel.loadData("/weather/historicHumidities?year="+this.year+"&month="+this.month)
+		sap.ui.getCore().setModel(tempModel, "humidityModel");
+	},
+
+	handleNavButtonPress: function(oEvent) {
+		sap.ui.getCore().getEventBus().publish("nav", "back");
+	}
+
+});
