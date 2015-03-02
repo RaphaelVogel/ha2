929f3bc8fd79b4292d5a60db97dbb4d83d3e31af
D026812 <raphael.vogel@sap.com>
2015-03-02 16:55:46 +0100

    add ui5lib directory

--- web/remove_dbg_ui5.py
+++ web/remove_dbg_ui5.py
@@ -1,6 +1,6 @@
 # Can be used to remove the -dbg files from SAP UI5 if it should be deployed
 import os
-rootDir = '.'
+rootDir = 'ui5lib'
 for dirName, subdirList, fileList in os.walk(rootDir):
     for fname in fileList:
         if '-dbg' in fname:
